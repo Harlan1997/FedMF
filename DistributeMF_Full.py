eacde1bcc6c4e1c0b38d5e2e2b4918c0bbcb3c01
Harlan1997 <11711015@mail.sustech.edu.cn>
2022-03-31 03:18:40 +0800

    _

--- DistributeMF_Full.py
+++ DistributeMF_Full.py
@@ -36,7 +36,7 @@ def make_print_to_file(path='./'):
 
 def user_update(single_user_vector, user_rating_list, item_vector):
     gradient = np.zeros([len(item_vector), len(single_user_vector)])
-    for item_id, rate in user_rating_list:
+    for item_id, rate, _ in user_rating_list:
         error = rate - np.dot(single_user_vector, item_vector[item_id])
         single_user_vector = single_user_vector - lr * (-2 * error * item_vector[item_id] + 2 * reg_u * single_user_vector)
         gradient[item_id] = error * single_user_vector
